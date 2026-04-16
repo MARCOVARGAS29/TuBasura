5a7b3c83722b40fa8ff965f3c64f14aafdf44447
Marco <jhean.vargas@ucb.edu.bo>
2026-04-16 16:23:03 -0400

    test(red): crear opciones de distrito

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,9 +1,11 @@
-import sumar from "./sumador.js";
+import { createDistrictOptions } from './model.js';
 
-describe("Sumar", () => {
-  it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(5);
+describe('CollectionScheduleModel', () => {
+  it('deberia crear 15 opciones de distrito', () => {
+    const options = createDistrictOptions();
+
+    expect(options).toHaveLength(15);
+    expect(options[0]).toEqual({ value: '1', label: 'Distrito 1' });
+    expect(options[14]).toEqual({ value: '15', label: 'Distrito 15' });
   });
 });
-
-
