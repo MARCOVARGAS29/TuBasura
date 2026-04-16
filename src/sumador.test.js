d672ba24de46a2a8505198c23f01bcb226c1d020
Marco <jhean.vargas@ucb.edu.bo>
2026-04-16 16:24:16 -0400

    test(red): consultar horario por distrito

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -1,4 +1,4 @@
-import { createDistrictOptions } from './model.js';
+import CollectionScheduleModel, { createDistrictOptions } from './model.js';
 
 describe('CollectionScheduleModel', () => {
   it('deberia crear 15 opciones de distrito', () => {
@@ -7,5 +7,15 @@ describe('CollectionScheduleModel', () => {
     expect(options).toHaveLength(15);
     expect(options[0]).toEqual({ value: '1', label: 'Distrito 1' });
     expect(options[14]).toEqual({ value: '15', label: 'Distrito 15' });
+  });
+
+  it('deberia devolver el horario del distrito seleccionado', () => {
+    const model = new CollectionScheduleModel();
+
+    expect(model.getScheduleByDistrict('5')).toEqual({
+      district: 'Distrito 5',
+      days: 'Lunes, miercoles y viernes',
+      time: '10:00',
+    });
   });
 });
